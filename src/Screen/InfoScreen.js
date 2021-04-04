250ae279d529d0be7ff4c7b2ee1e3f5217627c8e
DucPham17 <phamminhduckl@gmail.com>
2021-04-04 18:34:08 -0500

    completed info page

--- src/Screen/InfoScreen.js
+++ src/Screen/InfoScreen.js
@@ -1,19 +1,22 @@
-import React, { useState, useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { signout } from '../Action/userAction';
 import NavTabs from '../Component/NavTabs';
+import { Card } from 'react-bootstrap';
+import { Container } from '@material-ui/core';
 
 
 function InfoScreen(props) {
-    const userInfo = useSelector(state => state.signin);
+    const user = useSelector(state => state.signin);
     const dispatch = useDispatch();
-
+    const { loading, userInfo, error } = user;
 
     useEffect(() => {
-        if(!userInfo.userInfo){
+        if (!userInfo) {
             props.history.push("/");
         }
     })
+    console.log(userInfo);
     const handleSignout = () => {
         dispatch(signout());
     }
@@ -21,7 +24,19 @@ function InfoScreen(props) {
     return (
         <div>
             <NavTabs />
-            <button onClick={handleSignout}>Signout</button>
+            <Container className="signin-container">
+                <div className="w-100" style={{ maxWidth: "400px" }}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Text>Name: {userInfo.displayName}</Card.Text>
+                            <Card.Text>Email: {userInfo.email}</Card.Text>
+                            <button onClick={handleSignout}>Signout</button>
+                        </Card.Body>
+
+                    </Card>
+                </div>
+            </Container>
+
         </div>
     )
 }
